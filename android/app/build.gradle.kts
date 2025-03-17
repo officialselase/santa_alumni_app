73776dab82207c7dc45ea2c734f3fb1dbcfe3418
Sels <40667206+officialselase@users.noreply.github.com>
2025-03-17 12:36:54 +0000

    feat: Initial project setup: flutter pproject, splash assets, onboarding folder. Also complete splash screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.santa_alumni_app"
+    namespace = "com.santa_alumni_app.santa_alumni_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.santa_alumni_app"
+        applicationId = "com.santa_alumni_app.santa_alumni_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
